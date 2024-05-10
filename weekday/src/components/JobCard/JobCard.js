9885f58e5c5beaadf3b26d8fca41681a751f73da
yash537 <yashvijaypatel14@gmail.com>
2024-05-10 15:31:03 +0530

    Intregrated api for dynamic job card

--- weekday/src/components/JobCard/JobCard.js
+++ weekday/src/components/JobCard/JobCard.js
@@ -8,7 +8,7 @@ import Chip from "@mui/joy/Chip";
 import Typography from "@mui/joy/Typography";
 import "./JobCard.css";
 
-const JobCard = () => {
+const JobCard = ({ job }) => {
   return (
     <Card
       className="job-card"
@@ -22,31 +22,37 @@ const JobCard = () => {
         <Chip size="sm" variant="soft">
           ⏳ posted 13 days ago
         </Chip>
-        <Chip size="sm" variant="soft">
-          📈 1 referral
-        </Chip>
+        {job.referralCount && (
+          <Chip size="sm" variant="soft">
+            📈 {job.referralCount} referral
+          </Chip>
+        )}
       </Box>
       <Box className="card-header">
         <Box className="company-logo">
-          <img
-            src={""}
-            alt="logo"
-            style={{ borderRadius: "50%", objectFit: "contain" }}
-          />
+          {job.logoUrl && (
+            <img
+              src={job.logoUrl}
+              alt="logo"
+              style={{ borderRadius: "50%", objectFit: "contain" }}
+            />
+          )}
         </Box>
         <Box className="company-info">
           <Typography level="body-lg" className="company-name">
-            <a href={"#"} target="_blank" rel="noopener noreferrer">
-              weekday
+            <a href={job.jdLink} target="_blank" rel="noopener noreferrer">
+              {job.companyName}
             </a>
           </Typography>
-          <Typography>frontend</Typography>
-          <Typography level="body-sm">Remote</Typography>
+          <Typography>{job.jobRole}</Typography>
+          <Typography level="body-sm">{job.location}</Typography>
         </Box>
       </Box>
       <Box>
         <Typography level="body-sm">
-          Estimated Salary : $50k- $60k /yr
+          {job.minJdSalary &&
+            job.maxJdSalary &&
+            `Estimated Salary : $${job.minJdSalary}k- $${job.maxJdSalary}k /yr ✅`}
         </Typography>
       </Box>
       <CardContent>
@@ -54,24 +60,16 @@ const JobCard = () => {
         <Typography level="title-md">About us</Typography>
         <Box className="company-detail">
           <Typography className="company-about" level="body-md">
-            This is a sample job and you must have displayed it to understand
-            that its not just some random lorem ipsum text but something which
-            was manually written. Oh well, if random text is what you were
-            looking for then here it is: Lorem Ipsum is simply dummy text of the
-            printing and typesetting industry. Lorem Ipsum has been the
-            industry's standard dummy text ever since the 1500s, when an unknown
-            printer took a galley of type and scrambled it to make a type
-            specimen book. It has survived not only five centuries, but also the
-            leap into electronic typesetting, remaining essentially unchanged.
-            It was popularised in the 1960s with the release of Letraset sheets
-            containing Lorem Ipsum passages and now in this assignment.
+            {job.jobDetailsFromCompany || "No details available"}
           </Typography>
           <Typography level="body-md" className="view-job">
             View job
           </Typography>
         </Box>
         <Typography level="body-md">Minimum Experience</Typography>
-        <Typography level="body-md">2-3 years</Typography>
+        <Typography level="body-md">
+          {job.minExp && job.maxExp && `${job.minExp}-${job.maxExp}`} years
+        </Typography>
       </CardContent>
       <CardActions className="card-footer">
         <Button className="apply-btn">⚡ Easy Apply</Button>
--- weekday/src/components/JobList/JobList.js
+++ weekday/src/components/JobList/JobList.js
@@ -1,12 +1,70 @@
-import React from "react";
-import FilterJob from "../Filter/FilterJob";
+import React, { useEffect, useState } from "react";
 import JobCard from "../JobCard/JobCard";
+import { StyledEngineProvider, CssVarsProvider } from "@mui/joy/styles";
+import "./JobList.css";
+import FilterJob from "../Filter/FilterJob";
 
 const JobList = () => {
+  const [jobList, setJobs] = useState([]);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const [hasMore, setHasMore] = useState(true);
+
+  useEffect(() => {
+    loadJobs();
+  }, [page]);
+
+  const loadJobs = () => {
+    setLoading(true);
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    const body = JSON.stringify({
+      limit: 10,
+      offset: page,
+    });
+    const payload = {
+      method: "POST",
+      headers: myHeaders,
+      body,
+    };
+    fetch("https://api.weekday.technology/adhoc/getSampleJdJSON", payload)
+      .then((response) => response.json())
+      .then((data) => {
+        setJobs((prevJobs) => [...prevJobs, ...data.jdList]);
+        setHasMore(data.hasMore); // Assuming your API returns a flag indicating if there are more jobs
+        setLoading(false);
+        return data;
+      });
+  };
+
+  const handleScroll = () => {
+    if (
+      window.innerHeight + document.documentElement.scrollTop ===
+      document.documentElement.offsetHeight
+    ) {
+      if (!loading && hasMore) {
+        setPage((prevPage) => prevPage + 1);
+      }
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
     <>
-      <FilterJob />
-      <JobCard />
+      <StyledEngineProvider injectFirst>
+        <CssVarsProvider>
+          <FilterJob />
+          <div className="job-list ">
+            {jobList.map((job, index) => (
+              <JobCard job={job} key={job.jdUid + index} />
+            ))}
+          </div>
+        </CssVarsProvider>
+      </StyledEngineProvider>
     </>
   );
 };
